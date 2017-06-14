f6a15c1343e1a0bd8f966ea5f5421a9a4987debc
sajad1200 <sego.sego1122@gmail.com>
2017-06-15 00:30:37 +0300

    Create launch.sh

--- launch.sh
+++ launch.sh
@@ -1,18 +1,27 @@
-#!/usr/bin/env bash
-
-install() {
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get install g++-4.7 c++-4.7
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-sudo apt-get install screen
-sudo apt-get install tmux
+
+sudo apt-get upgrade -y
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev  -y
+
+sudo apt-get install lua-lgi -y
+
+sudo apt-get install software-properties-common -y
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+
+sudo apt-get install libstdc++6 -y
+
+sudo apt-get update -y
+
+sudo apt-get upgrade -y
+
+sudo apt-get dist-upgrade -y
+
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  tar zxpf luarocks-2.2.2.tar.gz
- mv luarocks-2.2.2 luarocks
- rm -rf 'luarocks-2.2.2.tar.gz'
- cd luarocks
+ rm luarocks-2.2.2.tar.gz
+ cd luarocks-2.2.2
  ./configure; sudo make bootstrap
  sudo luarocks install luasocket
  sudo luarocks install luasec
@@ -22,19 +31,20 @@ wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  sudo luarocks install dkjson
  sudo luarocks install lanes
  sudo luarocks install Lua-cURL
+ sudo service redis-server start
+ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+ sudo apt-get install g++-4.7 -y c++-4.7
+ sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+ sudo apt-get install screen -y
+ sudo apt-get install tmux -y
+ sudo apt-get install libstdc++6 -y
+ sudo apt-get install lua-lgi -y
+ sudo apt-get install libnotify-dev -y
+ sudo apt-get install python-setuptools python-dev build-essential -y
+ sudo easy_install pip
+ sudo pip install redis
+ cd ..
  cd NewBot
-  wget https://valtman.name/files/telegram-cli-1124
-mv telegram-cli-1124 tg
-chmod +x tg
-  cd ..
-}
-
-if [ "$1" = "install" ]; then
-  install
-else
-  while true; do
-  telegramseed_config
-  ./tg -s bot.lua 
-   sleep 3
-  done
-fi
+ rm -rf "luarocks-2.2.2"
+ wget "https://valtman.name/files/telegram-cli-1222"
+ mv telegram-cli-1222 tg
